c84b30f7f56740fef248631d9f67a6db0a648330
Andarov <bekturandarob678@gmail.com>
2022-05-26 18:57:17 +0500

    Add questions section and CTA

---
+++ main.js
@@ -0,0 +1,13 @@
+let elQuestionsItem = document.querySelectorAll(".js-questions-item");
+let elQuestionsText = document.querySelectorAll(".js-questions-text");
+let elQuestionsHeading = document.querySelectorAll(".questions__item-title");
+let elQuestionsIcon = document.querySelectorAll(".down-icon");
+
+elQuestionsItem.forEach(function(item,index) {
+    item.addEventListener("click", function () {
+        elQuestionsText[index].classList.toggle("visuallyhidden")
+        elQuestionsIcon[index].classList.toggle("fa-angle-up")
+        elQuestionsHeading[index].style.color = "red";
+        elQuestionsIcon[index].style.color = "red";
+    })
+})
